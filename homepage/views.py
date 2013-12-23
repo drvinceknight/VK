b9412dc7c5ff506a496a517a0038c434ab01eae7
vince <vincent.knight@gmail.com>
2013-12-23 22:39:11 +0000

    downloaded css file...

--- homepage/views.py
+++ homepage/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def index(request):
-    return HttpResponse("At the homepage of VK")
+    return render_to_response('homepage/index.html')
