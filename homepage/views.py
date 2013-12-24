bd869936a8bb61e7cf07daeaf70d2f6965116b3e
vince <vincent.knight@gmail.com>
2013-12-24 13:29:39 +0000

    extracted youtube id from url I think...

--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 from git import Repo
+from urlparse import urlparse
 
 from vids.models import Video
 
@@ -8,7 +9,7 @@ def index(request):
     latest_video_list = Video.objects.all().order_by('-pub_date')[:5]
 
     latest_commits = [c.message for c in Repo("./").iter_commits('Head', max_count=5)]
-    print latest_commits
+
 
     context = {'latest_video_list': latest_video_list, 'latest_commits': latest_commits}
     return render_to_response('homepage/index.html', context)
