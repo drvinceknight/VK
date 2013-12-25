a0b73c8d0137c48629825e78896b87004653fce2
vince <vincent.knight@gmail.com>
2013-12-25 08:18:32 +0000

    Trying to sort out problem with research table

--- VK/settings.py
+++ VK/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = (
     'homepage',
     'vids',
     'teaching',
+    'research',
 )
 
 MIDDLEWARE_CLASSES = (
--- research/models.py
+++ research/models.py
@@ -1,3 +1,21 @@
+import datetime
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class Paper(models.Model):
+    title = models.CharField(max_length=500)
+    journal = models.CharField(max_length=500, blank=True)
+    volume = models.CharField(max_length=500, blank=True)
+    issue = models.CharField(max_length=500, blank=True)
+    pages = models.CharField(max_length=500, blank=True)
+    pub_date = models.DateField('publication date', blank=True)
+    authors = models.TextField()
+    abstract = models.TextField()
+    openaccess = models.BooleanField()
+    def published(self):
+        if not self.pub_date:
+            return False
+        now = timezone.now()
+        return self.pub_date <= now
+    def __unicode__(self):
+        return self.title
