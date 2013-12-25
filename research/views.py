eedd7c79431b791645717963d06bf08dcc8e2b0e
vince <vincent.knight@gmail.com>
2013-12-25 19:48:33 +0000

    got link working for individual paper

--- research/views.py
+++ research/views.py
@@ -1,10 +1,4 @@
+from django.http import HttpResponse
 
-from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import get_object_or_404, render
-from django.utils import timezone
-from django.views import generic
-
-
-def detail(request):
+def detail(request, paper_id):
     return HttpResponse("Hello world")
