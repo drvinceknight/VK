394f3a69e936342e74eeef61386c43ecbe1f551d
vince <vincent.knight@gmail.com>
2013-12-29 12:00:43 +0000

    tweaked views to have HEAD

--- homepage/views.py
+++ homepage/views.py
@@ -21,7 +21,7 @@ def index(request):
 
     latest_paper_list = Paper.objects.all().order_by('-pub_date')[:5]
 
-    latest_commits = [Commit(c)  for c in Repo("./").iter_commits('Head', max_count=5)]
+    latest_commits = [Commit(c)  for c in Repo("./").iter_commits('HEAD', max_count=5)]
 
     all_course_list = Course.objects.all().order_by('-title')
     current_course_list = [c for c in all_course_list if c.currently_taught()]
