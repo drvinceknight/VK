91c4105ab03cf93bb3471f75e260fc582a7ff61a
vince <vincent.knight@gmail.com>
2013-12-30 06:24:06 -0500

    Have a random graph being created

--- coolstuff/urls.py
+++ coolstuff/urls.py
@@ -4,4 +4,5 @@ from coolstuff import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index),
+    url(r'^randomplot.png$', views.randomplot),
     )
--- coolstuff/views.py
+++ coolstuff/views.py
@@ -30,3 +30,30 @@ def index(request):
                'latest_paper_list': latest_paper_list}
 
     return render_to_response('coolstuff/index.html', context)
+
+def randomplot(request):
+    import random
+    import django
+    import datetime
+
+    from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+    from matplotlib.figure import Figure
+    from matplotlib.dates import DateFormatter
+
+    fig=Figure()
+    ax=fig.add_subplot(111)
+    x=[]
+    y=[]
+    now=datetime.datetime.now()
+    delta=datetime.timedelta(days=1)
+    for i in range(10):
+        x.append(now)
+        now+=delta
+        y.append(random.randint(0, 1000))
+    ax.plot_date(x, y, '-')
+    ax.xaxis.set_major_formatter(DateFormatter('%Y-%m-%d'))
+    fig.autofmt_xdate()
+    canvas=FigureCanvas(fig)
+    response=django.http.HttpResponse(content_type='image/png')
+    canvas.print_png(response)
+    return response
