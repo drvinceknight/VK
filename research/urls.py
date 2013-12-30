74280e00964e90aa0105bae3767b481c5068e705
vince <vincent.knight@gmail.com>
2013-12-30 13:20:56 -0500

    Fixed link in the wrong place

--- research/urls.py
+++ research/urls.py
@@ -5,4 +5,5 @@ from research import views
 urlpatterns = patterns('',
     url(r'^$', views.index),
     url(r'^(?P<paper_id>\d+)/$', views.detail, name='detail'),
+    url(r'^publicationlist$', views.publicationlist, name='publicationlist'),
 )
