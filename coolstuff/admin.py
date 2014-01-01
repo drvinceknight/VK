52be2bba27c52c1b82500ab2f5c7d12bda7c85f7
vince <vincent.knight@gmail.com>
2014-01-01 11:15:43 +0000

    Added some useful links.

--- coolstuff/admin.py
+++ coolstuff/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+from coolstuff.models import UsefullLink
+
+admin.site.register(UsefullLink)
--- coolstuff/models.py
+++ coolstuff/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 
-class UsefullLink(models.Model)
+class UsefullLink(models.Model):
     title = models.CharField(max_length=500)
-    url = models.URLField(max_length=200, blank=True)
-    description = models.TextField()
+    url = models.URLField(max_length=500)
+    category = models.CharField(max_length=500,blank=True)
+    description = models.TextField(blank=True)
+    def __unicode__(self):
+        return self.title
