83125fd0aa59defc505d8e391b2abb5fd6338b4a
vince <vincent.knight@gmail.com>
2014-01-07 07:22:54 +0000

    Created blog model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=255, blank=True)
+    slug = models.SlugField(unique=True, max_length=255, blank=True)
+    description = models.CharField(max_length=255, blank=True)
+    content = models.TextField(blank=True)
+    published = models.BooleanField(default=True, blank=True)
+    pubdate = models.DateTimeField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __unicode__(self):
+        return u'%s' % self.title
+
+    def get_absolute_url(self):
+        return reverse('blog.views.post', args=[self.slug])
