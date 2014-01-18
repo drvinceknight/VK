e6bfc603150d0ea099ff1f4be5ea776cf46248a1
vince <vincent.knight@gmail.com>
2014-01-18 10:31:44 +0000

    Have basic model for letters of recommendation

--- coolstuff/admin.py
+++ coolstuff/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
 from coolstuff.models import UsefullLink
+from coolstuff.models import LettersOfRecommendation
 
 admin.site.register(UsefullLink)
+admin.site.register(LettersOfRecommendation)
