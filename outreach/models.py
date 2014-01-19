c91ca6e600dd0217d48f98288ba25ada71eff403
vince <vincent.knight@gmail.com>
2014-01-19 09:40:26 -0500

    Adding blog database

--- outreach/models.py
+++ outreach/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(max_length=255, blank=True)
+    description = models.CharField(max_length=255, blank=True)
+    published = models.BooleanField(default=True, blank=True)
+    posted = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-posted']
+
+    def __unicode__(self):
+        return u'%s' % self.title
