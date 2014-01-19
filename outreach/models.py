21b02ac5938bb12f130b479911b3039a53dddc45
vince <vincent.knight@gmail.com>
2014-01-19 09:57:39 -0500

    Have model for blog post

--- outreach/models.py
+++ outreach/models.py
@@ -4,10 +4,11 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=255, blank=True)
     description = models.CharField(max_length=255, blank=True)
     published = models.BooleanField(default=True, blank=True)
-    posted = models.DateTimeField(auto_now_add=True)
+    date_added = models.DateTimeField(blank=True, null=True)
+    url = models.URLField(max_length=500, blank=True)
 
     class Meta:
-        ordering = ['-posted']
+        ordering = ['-date_added']
 
     def __unicode__(self):
         return u'%s' % self.title
